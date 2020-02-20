94c367879466787528ead8fea38160c9ab1c20e6
JCamiloo <jcamilo.osorio15@gmail.com>
2020-02-19 22:31:14 -0500

    using helpers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,11 +11,11 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.render('home', { name: 'Juan', year: new Date().getFullYear()})
+  res.render('home', { name: 'juan camilo' });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { year: new Date().getFullYear()})
+  res.render('about');
 });
 
-app.listen(3000, () => console.log('listening'));
+app.listen(port, () => console.log(`listening on port ${port}`));
